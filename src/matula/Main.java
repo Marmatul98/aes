4b6d1be8ee47a1465048922de9b73c8d6bddc078
Marek <marek.matula98@gmail.com>
2021-11-20 14:53:52 +0100

    encryption and decryption should work II

--- src/matula/Main.java
+++ src/matula/Main.java
@@ -3,14 +3,10 @@ package matula;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Base64;
@@ -19,7 +15,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+    public static void main(String[] args) throws IllegalBlockSizeException, IOException, BadPaddingException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Zvolte režim:");
@@ -27,6 +23,7 @@ public class Main {
         System.out.println("2. Dešifrování");
 
         int type = scanner.nextInt();
+        scanner.nextLine();
         if (type == 1) {
             encrypt(scanner);
         } else if (type == 2) {
@@ -34,7 +31,7 @@ public class Main {
         }
     }
 
-    private static void decrypt(Scanner scanner) throws FileNotFoundException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private static void decrypt(Scanner scanner) throws IllegalBlockSizeException, IOException, BadPaddingException {
         String variation = getVariation(scanner);
 
         System.out.println("Zadejte tajný klíč:");
@@ -44,38 +41,96 @@ public class Main {
         byte[] ivAsBytes = getIvAsBytes(scanner);
         IvParameterSpec ivspec = new IvParameterSpec(ivAsBytes);
 
-        System.out.println("Zadejte zašifrovaný text:");
-        String word = scanner.nextLine();
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("AES/" + variation + "/PKCS5Padding");
+        } catch (Exception ignored) {
+            System.out.println("Režim AES nebyl nalezen");
+            System.exit(0);
+        }
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+        } catch (Exception ignored) {
+            System.out.println("Špatně zadaný klíč nebo inicializační vektor");
+            System.exit(0);
+        }
 
-        Cipher cipher = Cipher.getInstance("AES/" + variation + "/PKCS5Padding");
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(word));
-        System.out.println("Dešifrovaný text: " + new String(plainText));
+        decryptWord(scanner, cipher);
     }
 
-    private static byte[] getIvAsBytes(Scanner scanner) throws FileNotFoundException {
-        byte[] returned = new byte[16];
-
-        System.out.println("Zadejte cestu k .txt souboru s inicializačním vektorem");
-        StringBuilder data = new StringBuilder();
-        while (true) {
-            String path = scanner.nextLine();
+    private static void decryptWord(Scanner scanner, Cipher cipher) throws IOException, IllegalBlockSizeException, BadPaddingException {
+        int variation = getWordEntryVariation(scanner, "dešifrování");
+        if (variation == 1) {
+            System.out.println("Zadejte slovo k dešifrování: ");
+            String wordToEncrypt = scanner.nextLine();
+            byte[] decodedWord = Base64.getDecoder().decode(wordToEncrypt.getBytes());
             try {
-                File myObj = new File(path);
-                Scanner myReader = new Scanner(myObj);
-                while (myReader.hasNextLine()) {
-                    data.append(myReader.nextLine());
-                }
-                myReader.close();
+                byte[] decrypted = cipher.doFinal(decodedWord);
+                System.out.println("Dešifrované slovo: " + new String(decrypted));
+            } catch (Exception ignored) {
+                System.out.println("Nepodařilo se dešifrovat slovo");
+            }
+        } else {
+            encryptOrDecryptFile(scanner, cipher, "decrypted");
+        }
+    }
+
+    private static void encryptOrDecryptFile(Scanner scanner, Cipher cipher, String encryptedOrDecrypted) throws IOException, IllegalBlockSizeException, BadPaddingException {
+        String decryptOrEncryptInCzech = "";
+        String decryptOrEncryptInCzech2 = "";
+        if (encryptedOrDecrypted.equals("encrypted")) {
+            decryptOrEncryptInCzech = "zašifrování";
+            decryptOrEncryptInCzech2 = "zašifrované";
+        } else if (encryptedOrDecrypted.equals("decrypted")) {
+            decryptOrEncryptInCzech = "dešifrování";
+            decryptOrEncryptInCzech2 = "dešifrované";
+        }
+
+        System.out.println("Zadejte cestu k .txt souboru s textem k " + decryptOrEncryptInCzech);
+        File file;
+        while (true) {
+            try {
+                String path = scanner.nextLine();
+                file = new File(path);
                 break;
             } catch (Exception ignored) {
                 System.out.println("Soubor nebyl nalezen, zkuste zadat znovu");
             }
         }
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Soubor nebyl nalezen, zkuste zadat znovu");
+        }
+        FileOutputStream outputStream = new FileOutputStream(encryptedOrDecrypted + ".txt");
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+        System.out.println("Slovo bylo " + decryptOrEncryptInCzech2 + " do souboru " + encryptedOrDecrypted + ".txt");
+    }
+
+
+    private static byte[] getIvAsBytes(Scanner scanner) {
+        byte[] returned = new byte[16];
+
+        System.out.println("Zadejte cestu k .txt souboru s inicializačním vektorem");
+        String data = getTxtFile(scanner);
         List<String> ivList;
         while (true) {
-            if (data.toString().startsWith("[") && data.toString().endsWith("]")) {
+            if (data.startsWith("[") && data.endsWith("]")) {
                 ivList = Arrays.asList(data.substring(1, data.length() - 1).split(", "));
             } else {
                 System.out.println("Zadejte pole ve správném formátu - [čislo, číslo, číslo, ..., číslo]");
@@ -91,12 +146,17 @@ public class Main {
         return returned;
     }
 
-    private static void encrypt(Scanner scanner) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+    private static void encrypt(Scanner scanner) throws IllegalBlockSizeException, IOException, BadPaddingException {
         IvParameterSpec ivspec = new IvParameterSpec(getRandomIV());
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter("iv.txt"));
-        writer.write((Arrays.toString(ivspec.getIV())));
-        writer.close();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("iv.txt"))) {
+            writer.write((Arrays.toString(ivspec.getIV())));
+        } catch (Exception ignored) {
+            System.out.println("Soubor s inicializačním vektorem se nepodařilo vytvořit");
+            System.exit(0);
+        }
+
+        System.out.println("Do složky s .jar souborem byl vytvořen iv.txt soubor s inicializačním vektorem");
 
         System.out.println("Zvolte délku šifrovacího klíče:");
         System.out.println("1. 128");
@@ -104,6 +164,7 @@ public class Main {
         System.out.println("3. 256");
 
         int length = scanner.nextInt();
+        scanner.nextLine();
         String key;
         switch (length) {
             case 1:
@@ -121,27 +182,87 @@ public class Main {
 
         String variation = getVariation(scanner);
 
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
-        Cipher cipher = Cipher.getInstance("AES/" + variation + "/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+        SecretKeySpec secretKey = null;
+        Cipher cipher = null;
+        try {
+            secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+            cipher = Cipher.getInstance("AES/" + variation + "/PKCS5Padding");
+        } catch (Exception ignored) {
+            System.out.println("Režim AES nebyl nalezen");
+            System.exit(0);
+        }
 
-        System.out.println("Zadejte slovo k zašifrování");
-        String wordToEncrypt = scanner.nextLine();
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+        } catch (Exception ignored) {
+            System.out.println("Nepodařilo se inicializovat šifrovací algoritmus");
+            System.exit(0);
+        }
 
-        System.out.println("Zašifrované slovo: " + Base64.getEncoder().encodeToString(cipher.doFinal(wordToEncrypt.getBytes(StandardCharsets.UTF_8))));
+        encryptWord(scanner, cipher);
+    }
+
+    private static void encryptWord(Scanner scanner, Cipher cipher) throws IOException, IllegalBlockSizeException, BadPaddingException {
+        int variation = getWordEntryVariation(scanner, "zašifrování");
+        if (variation == 1) {
+            System.out.println("Zadejte text k zašifrování");
+            String wordToEncrypt = scanner.nextLine();
+            String encrypted = Base64.getEncoder().encodeToString(cipher.doFinal(wordToEncrypt.getBytes(StandardCharsets.UTF_8)));
+            System.out.println("Zakódované slovo: " + encrypted);
+        } else {
+            encryptOrDecryptFile(scanner, cipher, "encrypted");
+        }
+    }
+
+    private static String getTxtFile(Scanner scanner) {
+        StringBuilder data = new StringBuilder();
+        while (true) {
+            String path = scanner.nextLine();
+            try {
+                File myObj = new File(path);
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    data.append(myReader.nextLine());
+                }
+                myReader.close();
+                break;
+            } catch (Exception ignored) {
+                System.out.println("Soubor nebyl nalezen, zkuste zadat znovu");
+            }
+        }
+        return data.toString();
+    }
+
+    private static int getWordEntryVariation(Scanner scanner, String encryptOrDecrypt) {
+        System.out.println("Zvolte režim zádání slova k " + encryptOrDecrypt);
+        System.out.println("1. Napsat do konzole");
+        System.out.println("2. Vložit .txt soubor");
+        int variation;
+        while (true) {
+            variation = scanner.nextInt();
+            scanner.nextLine();
+            switch (variation) {
+                case 1:
+                case 2:
+                    return variation;
+                default:
+                    System.out.println("Zadejte 1 nebo 2");
+            }
+        }
     }
 
     private static String getVariation(Scanner scanner) {
         System.out.println("Vyberte režím AES");
         System.out.println("1. CBC");
         System.out.println("2. CFB");
-        String variation;
+        int variation;
         while (true) {
-            variation = scanner.nextLine();
+            variation = scanner.nextInt();
+            scanner.nextLine();
             switch (variation) {
-                case "1":
+                case 1:
                     return "CBC";
-                case "2":
+                case 2:
                     return "CFB";
                 default:
                     System.out.println("Zadejte 1 nebo 2");
